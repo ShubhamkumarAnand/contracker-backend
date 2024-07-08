e7a68bc89b428b803de6664abb6d0b9f927126ac
imskanand <the.anand3099@gmail.com>
2024-07-08 21:14:36 +0530

    feat: intial api routes

--- main.go
+++ main.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Builder")
-}
-
 func main() {
-	http.HandleFunc("/builder", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Building a REST API in GoLang!")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Welcome to contracker")
+	})
+
+	mux.HandleFunc("POST /labours/add-labour", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Adding a new labour")
+	})
+
+	mux.HandleFunc("GET /labours", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Getting details of all the labours present...")
+	})
+
+	mux.HandleFunc("GET /labours/{id}", func(w http.ResponseWriter, r *http.Request){
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "Details of labour with id: %s", id)
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Println(err.Error())
+	}
 }
