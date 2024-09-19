90325f5635b4ba8fd838e8ea0f19d22d13dcb513
imskanand <the.anand3099@gmail.com>
2024-09-19 20:48:42 +0530

    fix: Employee Deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesController } from './employees/employees.controller';
 import { EmployeesModule } from './employees/employees.module';
+import { EmployeesService } from './employees/employees.service';
 
 @Module({
   imports: [DatabaseModule, EmployeesModule],
   controllers: [EmployeesController],
+  providers: [EmployeesService],
 })
 export class AppModule {}
