e03de6f853be95899ef8ecf4eb4f4cd7fe640c97
imskanand <the.anand3099@gmail.com>
2024-09-19 20:33:29 +0530

    Refacotor: Removed all the unwanted modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesController } from './employees/employees.controller';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [],
+  imports: [DatabaseModule, EmployeesModule],
+  controllers: [EmployeesController],
 })
 export class AppModule {}
