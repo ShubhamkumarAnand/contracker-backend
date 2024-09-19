17892b03a117d43af75c797ba98ff7231bee9ce1
imskanand <the.anand3099@gmail.com>
2024-09-19 14:04:20 +0530

    feat: REST API for the employees
    - Added prisma orm
    - Created REST API and performed CRUD operations on employees
    - Testing API ✅

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class EmployeesService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.databaseService.employee.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      const roleArray = await this.databaseService.employee.findMany({
+        where: {
+          role: role,
+        },
+      });
+      if (!roleArray.length) throw new NotFoundException('Role Not Found');
+      return roleArray;
+    }
+    return this.databaseService.employee.findMany();
+  }
+
+  async findOne(id: number) {
+    const employee = await this.databaseService.employee.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!employee) throw new NotFoundException('Employee Not Found');
+    return employee;
+  }
+
+  async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    const employee = await this.findOne(id);
+    if (!employee) throw new NotFoundException('Invalid Credentials');
+    this.databaseService.employee.update({
+      where: {
+        id: id,
+      },
+      data: updateEmployeeDto,
+    });
+    return { id, ...updateEmployeeDto };
+  }
+
+  async remove(id: number) {
+    const employee = await this.findOne(id);
+    if (!employee) throw new NotFoundException('Employee Not Found');
+    await this.databaseService.employee.delete({
+      where: {
+        id: id,
+      },
+    });
+    return employee;
+  }
+}
